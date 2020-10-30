c80b12a4c57e4f8dc1eb299d4dba7bfb02614fa8
Martin Kubica <martinko29.km@gmail.com>
2020-10-30 17:42:23 +0100

    +

--- Open-Lab-04.14/FramePrinter.cs
+++ Open-Lab-04.14/FramePrinter.cs
@@ -6,7 +6,35 @@ namespace Open_Lab_04._14
     {
         public void Print(string[] strings)
         {
-            throw new NotImplementedException();
+            int b = 0;
+            string h = "*";
+            for (int i = 0; i < strings.Length; i++)
+            {
+                if (strings[i].Length > b)
+                {
+                    b = strings[i].Length;
+                }
+            }
+            for (int i = 1; i < b + 4; i++)
+            {
+                Console.Write(h);
+            }
+            Console.WriteLine(h);
+            for (int i = 0; i < strings.Length; i++)
+            {
+                Console.Write(h + " ");
+                Console.Write(strings[i]);
+                for (int s = 0; s < b - strings[i].Length; s++)
+                {
+                    Console.Write(" ");
+                }
+                Console.WriteLine(" " + h);
+            }
+            for (int i = 1; i < b + 4; i++)
+            {
+                Console.Write(h);
+            }
+            Console.WriteLine(h);
         }
     }
 }
